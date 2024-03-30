199ecb51cd832cb56632841431449bc58a9c6176
Viateur <akimanaviateur94@gmail.com>
2024-03-30 14:42:25 +0200

    initial commit

--- main.js
+++
@@ -1,8 +0,0 @@
-var viateur = {
-    name: "viateur",
-    age: 90,
-    getDetails: function () {
-        return "hello ".concat(viateur.name, " with this age ").concat(viateur.age);
-    },
-};
-console.log(viateur.getDetails());
